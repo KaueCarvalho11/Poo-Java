89b4999926285156b531853b7dd8a9e53e0ab386
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-05 19:34:16 -0300

    Calculando o fatorial de um número com o laço for

---
+++ fatorial/calcula.java
@@ -0,0 +1,20 @@
+package fatorial;
+
+import java.util.Scanner;
+
+public class calcula {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe um número inteiro: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        int auxiliar = 1;
+        for(int i = 2; i <= n; i++){
+            auxiliar *= i;
+        }
+        System.out.println("Fatorial de " + n + " = " + auxiliar);   
+    }
+}
+
