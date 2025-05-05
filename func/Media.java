55ed5849015313e0b80e47d0266bf91862de77b9
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-05 19:56:26 -0300

    Trabalhando com funções

---
+++ func/Media.java
@@ -0,0 +1,25 @@
+package func;
+
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe sua primeira nota :");
+        double n1 = scanner.nextDouble();
+
+        System.out.print("Informe sua segunda nota: ");
+        double n2 = scanner.nextDouble();
+
+        System.out.print("Informe sua terceira nota: ");
+        double n3 = scanner.nextDouble();
+
+        System.out.println(media(n1, n2, n3));
+    }
+
+    public static double media(double num1, double num2, double num3){
+        return (num1 + num2 + num3) / 3;
+    }
+}
