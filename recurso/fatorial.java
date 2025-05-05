2f565015aaaf0802fb5b4eaa30948f6d1a5ea2c0
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-05 20:14:08 -0300

    Trabalhando com funções recursivas

---
+++ recurso/fatorial.java
@@ -0,0 +1,23 @@
+package recurso;
+
+import java.util.Scanner;
+
+public class fatorial {
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe um número: ");
+        double n = scanner.nextDouble();
+
+        System.out.print(recursao(n));
+    }
+
+    public static Double recursao(double num){
+        
+        if (num == 0.0){
+            return 1.0;
+        }
+        return num * recursao(num-1);
+    }
+}
