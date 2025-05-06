eaa003309b16c4ae47b61e189d887c9795b1d5b4
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-06 12:17:01 -0300

    Trabalhando com operadores aritméticos

---
+++ lista_introducao_java/questao005/Cdu.java
@@ -0,0 +1,19 @@
+package lista_introducao_java.questao005;
+
+import java.util.Scanner;
+
+public class Cdu {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Informe um número no formato CDU: ");
+        int numero = scanner.nextInt();
+        scanner.close();
+
+        int centena = numero / 100;
+        int dezena = (numero % 100) / 10;
+        int unidade = (numero % 100) % 10;
+
+        System.out.print("UDC = " + unidade + dezena + centena);
+    }
+}
