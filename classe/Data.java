0e0a040a36f35e3f32951dfcfb2eeddc1ed673ff
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-06 21:51:33 -0300

    Modelando classe data, atribuindo atributos e métodos

---
+++ classe/Data.java
@@ -0,0 +1,21 @@
+package classe;
+
+public class Data{
+    int dia;
+    int mes;
+    int ano;
+
+    public boolean dataEhValida(){
+        return ((dia > 0 && dia <= 30) && (mes > 0 && mes <=12) &&(ano >= 0));
+    }
+
+    public void inicializa(int d, int m, int a){
+        dia = d;
+        mes = m;
+        ano = a;    
+    }
+
+    public void mostraData(){
+        System.out.print(dia + "/" + mes + "/" +  ano);
+    }
+}
