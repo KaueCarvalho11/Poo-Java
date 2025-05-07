1b0071800b62ac2de504788cb20bd652b2efba3b
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-07 20:36:34 -0300

    Instanciando e fazendo a chamada de métodos da classe EventoAcademico

---
+++ evento/demoEventoAcademico.java
@@ -0,0 +1,9 @@
+package evento;
+
+public class demoEventoAcademico {
+    public static void main(String[]args){
+        EventoAcademico evento = new EventoAcademico("ECOP", "Auditório CC", 150);
+
+        evento.mostraEvento();
+    }
+}
