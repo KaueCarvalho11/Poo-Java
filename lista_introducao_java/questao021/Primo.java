18057d8e513b6e2005ca4f347280d8e38c12eff8
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-12 08:09:13 -0300

    Calculando número primo usando recursão

--- lista_introducao_java/questao021/Primo.java
+++ lista_introducao_java/questao021/Primo.java
@@ -1,32 +1,45 @@
 package lista_introducao_java.questao021;
 
-import java.util.Scanner;
+/*
+ Permitir entrada via teclado
+ Esse numero será passado como parâmetro para a função recursiva, para que, nas condicionais, ele possa se dividir,sendo no segundo parâmetro decrementado
+ A ideia é dividir o número por ele mesmo e por todos os números anteriores a ele
+ */
+
+ import java.util.Scanner;
 
 public class Primo {
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Informe um número: ");
-        int numero = scanner.nextInt();
+        int numero;
+        boolean ehPrimo;
+
+        System.out.print("Informe um número maior que 1: ");
+        numero = scanner.nextInt();
+
         scanner.close();
 
-        boolean isPrime = primo(numero, numero);
-        System.out.println(numero + " é primo? " + isPrime);
+        ehPrimo = primo(numero, numero);
+
+        System.out.print(numero + " é primo? " + ehPrimo);
+
     }
 
-    public static boolean primo(int n, int i) {
-        
-        if (i <= 1) {
-            return n > 1;
+    public static boolean primo(int n1, int n2){
+
+        if(n2 == n1){
+            return primo(n1, n2-1);
         }
 
-        if (i == n) {
-            return primo(n, i - 1);
+        if(n2 <=1){
+            return n1 > 1;
         }
-        
-        if (n % i == 0) {
+
+         if(n1 % n2 == 0){
             return false;
         }
-    
-        return primo(n, i - 1);
+
+        return primo(n1, n2-1);
     }
+
 }
