722ca3d9617066f47c091e1b2356eba114b64110
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-12 10:43:29 -0300

    Instanciando classe Time usando o construtor criado

---
+++ lista_criacao_classes_I/questao030/DemoTime.java
@@ -0,0 +1,8 @@
+package lista_criacao_classes_I.questao030;
+
+public class DemoTime {
+    public static void main(String[] args) {
+        Time vasco = new Time("vasco", "Rio de Janeiro", 12, 1898, 33, "Campeonato carioca");
+        vasco.exibir();
+    }
+}
