0ac3d831afc0f740a3c26222015c7be7631898e1
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-13 10:02:57 -0300

    Trabalhando com polinômios recursivamente

---
+++ lista_introducao_java/questao022/Polinomio.java
@@ -0,0 +1,38 @@
+package lista_introducao_java.questao022;
+
+import java.util.Scanner;
+
+import java.text.DecimalFormat;
+
+public class Polinomio{
+    public static void main(String[]args){
+        DecimalFormat df = new DecimalFormat("#.00");
+        Scanner input = new Scanner(System.in);
+        int n;
+        double x;
+
+        System.out.print("Informe o grau do polinômio: ");
+        n = input.nextInt();
+
+        System.out.print("Informe o valor de x: ");
+        x = input.nextDouble();
+
+        double valor = recursao(n, x, input);
+
+        System.out.print("P" + n + "(" + x + ") = " + valor);
+    }
+
+    public static double recursao(int n, double x, Scanner input){
+            System.out.print("Coeficiente a" + n + ":");
+            double coeficiente = input.nextDouble();
+
+            if(n == 0){
+                return coeficiente;
+            }
+
+            double anterior = recursao(n -1, x, input);
+
+            return x * anterior + coeficiente;
+
+        }
+}   
