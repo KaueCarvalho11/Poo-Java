8ec86fed5bf6b77f2aac48ed5886394979b82fdc
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-12 23:14:44 -0300

    Instanciando classe Aluno com o construtor criado

---
+++ lista_criacao_classes_I/questao032/AlunoDemo.java
@@ -0,0 +1,8 @@
+package lista_criacao_classes_I.questao032;
+
+public class AlunoDemo{
+    public static void main(String[]args){
+        Aluno kaue = new Aluno("2023011314", "Kauê", 6, 6, 6.8);
+        kaue.exibir();
+    }
+}
