90a1aed70639b6a2409027ecef4f064d84211421
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-22 13:44:50 -0300

    Testando métodos da classe Metodos

--- biblioteca/MetodoDemo.java
+++ biblioteca/MetodoDemo.java
@@ -2,6 +2,10 @@ package biblioteca;
 
 public class MetodoDemo{
     public static void main(String[] args) {
-        System.out.print("");
+        System.out.println("2 polegads convertidas para centimetros: " + Metodos.polegadasParaCentimetros(2));
+
+        System.out.println("3 pes convertidas para centimetros: " + Metodos.pesParaCentimetros(3));
+
+        System.out.println("10 milhas convertidas para centimetros: " + Metodos.milhasParaCentimetros(10));
     }
 }
