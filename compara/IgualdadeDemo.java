eff60ba73b9ff1d84f0146d7f90943bbdd046ca0
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-24 09:51:41 -0300

    Trabalhando com métodos genéricos

---
+++ compara/IgualdadeDemo.java
@@ -0,0 +1,13 @@
+public class IgualdadeDemo {
+    public static void main(String[]args){
+       System.out.println(Igualdade.ehIgual(12, 12));
+
+       System.out.println(Igualdade.ehIgual(12, 13));
+
+       System.out.println(Igualdade.ehIgual(true, true));
+
+       System.out.println(Igualdade.ehIgual("olá", "olá"));
+
+       System.out.println(Igualdade.ehIgual(12.1, 12.2));
+    }
+}
