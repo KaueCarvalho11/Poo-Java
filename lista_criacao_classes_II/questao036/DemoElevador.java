e66006d5c334d851fbf0754a7f6ed3de9543d8fd
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-24 09:53:35 -0300

    Demonstrando classe Elevador

---
+++ lista_criacao_classes_II/questao036/DemoElevador.java
@@ -0,0 +1,97 @@
+package lista_criacao_classes_II.questao036;
+
+public class DemoElevador{
+   public static void main(String[]args){
+    Elevador e1 = new Elevador(6, 7);
+
+    System.out.println(e1);
+
+    e1.entra();
+    System.out.println(e1);
+
+    e1.entra();
+    System.out.println(e1);
+
+    e1.entra();
+    System.out.println(e1);
+    
+    e1.entra();
+    System.out.println(e1);
+    
+    e1.entra();
+    System.out.println(e1);
+    
+    e1.entra();
+    System.out.println(e1);
+    
+    e1.entra();
+    System.out.println(e1);
+
+    e1.sai();
+    System.out.println(e1);
+    
+    e1.sai();
+    System.out.println(e1);
+    
+    e1.sai();
+    System.out.println(e1);
+    
+    e1.sai();
+    System.out.println(e1);
+    
+    e1.sai();
+    System.out.println(e1);
+    
+    e1.sai();
+    System.out.println(e1);
+    
+    e1.sai();
+    System.out.println(e1);
+
+    e1.sobe();
+    System.out.println(e1);
+    
+    e1.sobe();
+    System.out.println(e1);
+    
+    e1.sobe();
+    System.out.println(e1);
+    
+    e1.sobe();
+    System.out.println(e1);
+    
+    e1.sobe();
+    System.out.println(e1);
+    
+    e1.sobe();
+    System.out.println(e1);
+    
+    e1.sobe();
+    System.out.println(e1);
+    
+    e1.sobe();
+    System.out.println(e1);
+
+    e1.desce();
+    System.out.println(e1);
+    
+    e1.desce();
+    System.out.println(e1);
+    
+    e1.desce();
+    System.out.println(e1);
+    
+    e1.desce();
+    System.out.println(e1);
+    
+    e1.desce();
+    System.out.println(e1);
+    
+    e1.desce();
+    System.out.println(e1);
+    
+    e1.desce();
+    System.out.println(e1);
+   }
+    
+}
