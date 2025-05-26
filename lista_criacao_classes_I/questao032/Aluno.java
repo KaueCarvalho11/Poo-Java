e9182f3763cfa7b859d5f5180e7ce999a28dc2e0
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-05-26 15:11:08 -0300

    Update

--- lista_criacao_classes_I/questao032/Aluno.java
+++ lista_criacao_classes_I/questao032/Aluno.java
@@ -1,19 +1,13 @@
 package lista_criacao_classes_I.questao032;
 
-import java.text.DecimalFormat;
-
 public class Aluno{
-    String matricula;
-    String nome;
-    double prova1;
-    double prova2;
-    double trabalho;
-    double mediaPonderada;
-    double notaNecessaria;
-    double mediaFinal;
+    private int matricula;
+    private String nome;
+    private double prova1;
+    private double prova2;
+    private double trabalho;
 
-    Aluno(String matricula, String nome, double prova1, double prova2, double trabalho){
-
+    public Aluno(int matricula, String nome, double prova1, double prova2, double trabalho){
         this.matricula = matricula;
         this.nome = nome;
         this.prova1 = prova1;
@@ -22,48 +16,19 @@ public class Aluno{
     }
 
     public double media(){
-        return(((2.5 * prova1) +(2.5 * prova2) + (2 * trabalho))/7);
+        return (2.5 * prova1 + 2.5 * prova2 + 2.0 * trabalho)/7;
     }
 
     public double provaFinal(){
-        if(mediaPonderada < 3 || mediaPonderada >= 7){
+        if(media()<3|| media()>=7){
             return 0;
         }
-         return (50 - 6 * mediaPonderada) / 4; 
+        else return (50 - 6 * media())/4;
     }
 
-    public double mediaFim(){ 
-        return ((mediaPonderada * 6) + ((50 - 6 * mediaPonderada)*4))/10;
-    }
-
-    public void exibir(){
-        DecimalFormat df = new DecimalFormat("#.0");
-
-        System.out.println("Matrícula: " + matricula);
-        System.out.println("Nome: " + nome);
-        System.out.println("Nota prova 1: " +  prova1);
-        System.out.println("Nota prova 2: " + prova2);
-        System.out.println("Nota trabalho: " + trabalho);
-
-        mediaPonderada = media();
-        System.out.println("Média ponderada: " + df.format(mediaPonderada));
-
-        if(mediaPonderada < 3 || mediaPonderada>= 7){
-            System.out.println("Sem necessidade de realizar o exame final");
-
-        } else{
-            System.out.println("Vai realizar o exame final");
-            notaNecessaria = provaFinal();
-            System.out.println("Nota necessária para passar: " + df.format(notaNecessaria));
-
-            mediaFinal = mediaFim();
-            System.out.println("Média final: " + df.format(mediaFinal));
-
-            if(mediaFinal >=5){
-                System.out.println("Aprovado");
-            } else{
-                System.out.println("Reprovado");
-            }   
-        }
+    public double mediaFinal(){
+        if (media()>= 7 || media() < 3){
+            return media();
+        } else return (media() * 6 + (50 - 6 * media())/4)/10;  
     }
 }
--- lista_criacao_classes_I/questao032/AlunoDemo.java
+++ lista_criacao_classes_I/questao032/AlunoDemo.java
@@ -2,7 +2,17 @@ package lista_criacao_classes_I.questao032;
 
 public class AlunoDemo{
     public static void main(String[]args){
-        Aluno kaue = new Aluno("2023011314", "Kauê", 6, 6, 6.8);
-        kaue.exibir();
+
+    Aluno kaue = new Aluno(123, "kaue", 7, 7, 7);
+
+    System.out.println(kaue.media());
+
+    System.out.println(kaue.provaFinal());
+
+    System.out.println(kaue.mediaFinal());
+
+    if(kaue.mediaFinal() >= 5){
+        System.out.println("Aprovado");
+    } else System.out.print("Reprovado");
     }
 }
