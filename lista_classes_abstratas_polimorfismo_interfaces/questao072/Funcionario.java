3090a28d47f29cf7b5887172108a4d1d2cbbbfa0
Kauê Carvalho <kauecarvalho2028@gmail.com>
2025-06-23 09:19:34 -0300

    Trabalhando com polimorfismo de sobrecarga

--- lista_classes_abstratas_polimorfismo_interfaces/questao072/Funcionario.java
+++ lista_classes_abstratas_polimorfismo_interfaces/questao072/Funcionario.java
@@ -1,3 +1,45 @@
 public class Funcionario {
-  
+  private String nome;
+  private int idade;
+  private double salarioPorHora;
+
+  public Funcionario(String nome){
+    this.nome = nome;
+    idade = 20;
+    salarioPorHora = 2;
+  }
+
+  public Funcionario(String nome, double salarioPorHora){
+    this.nome = nome;
+    idade = 40;
+    this.salarioPorHora = salarioPorHora;
+  }
+
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+
+  public String getNome(){
+    return nome;
+  }
+
+  public void setIdade(int idade){
+    this.idade = idade;
+  }
+
+  public int getIdade(){
+    return idade;
+  }
+
+  public void setSalarioPorHora(double salarioPorHora){
+    this.salarioPorHora = salarioPorHora;
+  }
+
+  public double getSalarioPorHora(){
+    return salarioPorHora;
+  }
+
+  public String toString(){
+    return " nome  = " + nome + " idade = " + idade + " salário por hora trabalhada = " + salarioPorHora;
+  }
 }
