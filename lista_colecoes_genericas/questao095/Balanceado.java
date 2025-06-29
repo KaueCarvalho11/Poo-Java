426c9e9841777999fc2299969a2f183bfed7e324
Kauê Carvalho <147278152+KaueCarvalho11@users.noreply.github.com>
2025-06-28 23:31:36 -0300

    Trabalhando com árvores e método split da classe String

---
+++ lista_colecoes_genericas/questao095/Balanceado.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class Balanceado{
+    public static void main(String[]args){
+        Stack<String> pilha = new Stack<>();
+        String parenteses = "(())";
+        String vetor[] = parenteses.split("");
+
+        for(String v: vetor){
+            if(v.equals("(")){
+                pilha.push(v);
+            }else {
+                if(pilha.isEmpty()){
+                    System.out.println("Desbalanceada");
+                    return;
+                } else pilha.pop();
+            }
+        }
+
+        if (pilha.isEmpty()){
+            System.out.println("Balanceada");
+        } else System.out.println("Desbalanceada");
+    }
+}
