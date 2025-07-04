4d52be83b0bbc3714d008f5627c8733fbab38f96
KaueCarvalho11 <kauecarvalho2028@gmail.com>
2025-07-04 10:47:38 -0300

    Corrigindo formatação

--- respostas/Aluno.java
+++ respostas/Aluno.java
@@ -5,16 +5,16 @@ public class Aluno{
         int respostas[] = {1, 2, 5, 0, 3, 5, 2, 1, 3, 1, 14};
         int frequencia[] = new int[6];
 
-            for(int resp: respostas){
-                try{
+        for(int resp: respostas){
+            try{
                 frequencia[resp]++;
                            
-                }catch(IndexOutOfBoundsException e){
-            System.out.println(e.getMessage());
-            System.out.println(e);
-            System.out.println("Alternativa " + resp + " inválida");
-                }
+            }catch(IndexOutOfBoundsException e){
+                System.out.println(e.getMessage());
+                System.out.println(e);
+                System.out.println("Alternativa " + resp + " inválida");
             }
+        }
 
         System.out.println("Resposta\t Frequência");
         for(int i = 0; i < frequencia.length; i ++){
