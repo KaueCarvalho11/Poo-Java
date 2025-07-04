265e871cc6846bc9b913f78e1f0cc0bc846375fc
KaueCarvalho11 <kauecarvalho2028@gmail.com>
2025-07-04 10:46:19 -0300

    Trabalhando com try-catch

--- respostas/Aluno.java
+++ respostas/Aluno.java
@@ -2,12 +2,19 @@ package respostas;
 
 public class Aluno{
     public static void main(String[]args){
-        int respostas[] = {1, 2, 5, 0, 3, 5, 2, 1, 3, 1, 2};
+        int respostas[] = {1, 2, 5, 0, 3, 5, 2, 1, 3, 1, 14};
         int frequencia[] = new int[6];
 
-        for(int resp: respostas){
-            frequencia[resp]++;
-        }
+            for(int resp: respostas){
+                try{
+                frequencia[resp]++;
+                           
+                }catch(IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+            System.out.println(e);
+            System.out.println("Alternativa " + resp + " inválida");
+                }
+            }
 
         System.out.println("Resposta\t Frequência");
         for(int i = 0; i < frequencia.length; i ++){
