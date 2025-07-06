bd0803ec544b05294ab2ad2580eb963edc077e27
KaueCarvalho11 <kauecarvalho2028@gmail.com>
2025-07-06 13:33:51 +0000

    Trabalhando com diferentes tipos de exceções

--- lista_excecoes/questao103/Meses.java
+++ lista_excecoes/questao103/Meses.java
@@ -1,16 +1,28 @@
 package lista_excecoes.questao103;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Meses{
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        int valor;
+        int valor = 0;
         String vetor[] = {"janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
 
         System.out.print("Informe um valor inteiro: ");
-        valor = input.nextInt();
 
-        System.out.println(vetor[valor-1]);
+        try{
+            valor = input.nextInt();
+        }catch(InputMismatchException e){
+            System.out.println("Informe um valor inteiro");
+        }
+        
+
+        try{
+            System.out.println(vetor[valor-1]);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Valor inválido");
+        }
+        
         input.close();
     }
 }
